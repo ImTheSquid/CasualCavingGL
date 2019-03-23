d9cd7c6990abc72ea38894b8efbf740a4a2b6305
ImTheSquid <jackhogan11@gmail.com>
2019-03-23 17:57:57 -0400

    inital commit

---
+++ src/main/java/org/graphics/Render.java
@@ -0,0 +1,22 @@
+package org.graphics;
+
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+
+public class Render implements GLEventListener {
+    public void init(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    public void dispose(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    public void display(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
+
+    }
+}
---
+++ src/main/java/org/levels/Level.java
@@ -0,0 +1,4 @@
+package org.levels;
+
+public class Level {
+}
