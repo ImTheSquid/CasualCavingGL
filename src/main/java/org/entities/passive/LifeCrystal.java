885bb8546ec2e07f10aa16c40cdeb0201082c37c
ImTheSquid <jackhogan11@gmail.com>
2019-05-13 21:14:08 -0400

    Worked on life crystal, new golem sprites

--- src/main/java/org/entities/passive/LifeCrystal.java
+++ src/main/java/org/entities/passive/LifeCrystal.java
@@ -1,7 +1,9 @@
 package org.entities.passive;
 
 import org.entities.Autonomous;
+import org.graphics.Graphics;
 import org.level.LevelController;
+import org.loader.ResourceHandler;
 
 public class LifeCrystal extends Autonomous {
     public LifeCrystal(int subLevel, float spawnX, float spawnY) {
@@ -11,11 +13,14 @@ public class LifeCrystal extends Autonomous {
     @Override
     public void update() {
         if(health==0) LevelController.getCurrentLevel().getEntityRegister().add(new Health(subLevel,x,y));
+        if(damageTakenFrame>0)damageTakenFrame--;
     }
 
     @Override
     public void render() {
-
+        if(damageTakenFrame>0)Graphics.setColor(1,0,0,1);
+        else Graphics.setColor(1,1,1,1);
+        Graphics.drawImageCentered(ResourceHandler.getMiscLoader().getLifeCrystal(),x,y);
     }
 
     @Override
--- src/main/java/org/loader/entity/MiscLoader.java
+++ src/main/java/org/loader/entity/MiscLoader.java
@@ -3,9 +3,9 @@ package org.loader.entity;
 import org.loader.ImageResource;
 
 public class MiscLoader {
-    //private ImageResource lifeCrystal=new ImageResource("/");
+    private ImageResource lifeCrystal=new ImageResource("/CasualCaving/Entities/MiscEntities/Item_Stalagmite.png");
 
-    //public ImageResource getLifeCrystal(){return lifeCrystal;}
+    public ImageResource getLifeCrystal(){return lifeCrystal;}
 
     private ImageResource healthHeart=new ImageResource("/CasualCaving/Entities/Harold/Health_Heart.png");
 
