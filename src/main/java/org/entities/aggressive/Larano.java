abcd7123766e4276633430a6b82da5eb7781ff2b
ImTheSquid <jackhogan11@gmail.com>
2019-06-10 22:51:25 -0400

    Imported sprites for Larano

--- src/main/java/org/entities/aggressive/Larano.java
+++ src/main/java/org/entities/aggressive/Larano.java
@@ -3,6 +3,7 @@ package org.entities.aggressive;
 import org.entities.Autonomous;
 import org.entities.SmartRectangle;
 import org.graphics.Animator;
+import org.graphics.Graphics;
 import org.loader.ImageResource;
 import org.loader.ResourceHandler;
 
@@ -17,12 +18,22 @@ public class Larano extends Autonomous {
 
     @Override
     public void update() {
+        doSpriteCalc();
+    }
 
+    private void doSpriteCalc(){
+        sprite=larano.getCurrentFrame();
     }
 
     @Override
     public void render() {
-
+        if(sprite==null)return;
+        Graphics.setIgnoreScale(true);
+        width= Graphics.convertToWorldWidth(sprite.getTexture().getWidth());
+        height=Graphics.convertToWorldHeight(sprite.getTexture().getHeight());
+        hitbox.updateBounds(x,y,width,height);
+        Graphics.drawImage(sprite,x,y);
+        Graphics.setIgnoreScale(false);
     }
 
     @Override
@@ -34,6 +45,6 @@ public class Larano extends Autonomous {
 
     @Override
     public String toString() {
-        return null;
+        return "Larano @ "+x+","+y;
     }
 }
