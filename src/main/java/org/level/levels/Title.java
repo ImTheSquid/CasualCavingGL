448843c2bdecbc0ef9f3620db6eeb10b5f860d37
ImTheSquid <jackhogan11@gmail.com>
2019-06-13 22:22:25 -0400

    Updated version to 0.2.4

--- src/main/java/org/level/levels/Title.java
+++ src/main/java/org/level/levels/Title.java
@@ -163,7 +163,7 @@ public class Title extends Level {
         Graphics.drawTextCentered("Quit",Render.unitsWide/2,4f);
         Graphics.drawTextCentered("Controls",Render.unitsWide/2,8.3f);
         Graphics.setFont(Graphics.SMALL_FONT);
-        Graphics.drawText("Casual Caving 0.2.2",0.1f,0.7f);
+        Graphics.drawText("Casual Caving 0.2.4",0.1f,0.7f);
         Graphics.drawText("Lunan Productions",Render.unitsWide-Graphics.convertToWorldWidth((float)Graphics.getCurrentFont().getBounds("Lunan Productions").getWidth())-.1f,.7f);
         Graphics.drawImage(ResourceHandler.getMiscLoader().getMusicButton(AudioManager.isMusicEnabled()),0.5f,2,5,5);
 
