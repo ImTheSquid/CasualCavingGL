90d63c2cf372b89cddfbad13e97b95ec571e4dfe
ImTheSquid <jackhogan11@gmail.com>
2019-06-23 11:52:49 -0400

    Imported Inconsolata

--- src/main/java/org/engine/Main.java
+++ src/main/java/org/engine/Main.java
@@ -2,15 +2,31 @@ package org.engine;
 
 import org.entities.Harold;
 import org.graphics.Render;
+import org.loader.ResourceHandler;
+
+import java.awt.*;
+import java.io.IOException;
 
 public class Main {
     private static Harold harold=new Harold();
     public static void main(String[] args) {
+        initFonts();
         AudioManager.setup();
         new Render();
     }
 
     public static Harold getHarold() {
         return harold;
+    }
+
+    private static void initFonts(){
+        try{
+            GraphicsEnvironment ge=GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, ResourceHandler.getMiscLoader().getInconsolata().openStream()));
+        }catch (IOException e){
+            System.out.println("ERROR! No file found");
+        }catch (FontFormatException e){
+            System.out.println("ERROR! Incorrect font type");
+        }
     }
 }
--- src/main/java/org/loader/MiscLoader.java
+++ src/main/java/org/loader/MiscLoader.java
@@ -1,6 +1,8 @@
 package org.loader;
 
 
+import java.net.URL;
+
 public class MiscLoader {
     private ImageResource lifeCrystal=new ImageResource("/CasualCaving/Entities/MiscEntities/Item_Stalagmite.png");
 
@@ -13,4 +15,8 @@ public class MiscLoader {
     private ImageResource[] musicButton={new ImageResource("/CasualCaving/Menus/MusicON.png"),new ImageResource("/CasualCaving/Menus/MusicOFF.png")};
 
     public ImageResource getMusicButton(boolean on){return on?musicButton[0]:musicButton[1];}
+
+    public URL getInconsolata() {
+        return MiscLoader.class.getResource("/CasualCaving/Fonts/Inconsolata-Regular.ttf");
+    }
 }
