9fdb216e121b31225851497f9fa75751c0f874aa
ImTheSquid <jackhogan11@gmail.com>
2019-12-01 17:59:16 -0500

    Added some new assets

---
+++ src/main/java/org/entities/aggressive/Swolem.java
@@ -0,0 +1,24 @@
+package org.entities.aggressive;
+
+import org.entities.Autonomous;
+
+public class Swolem extends Autonomous {
+    public Swolem(int subLevel, float spawnX, float spawnY) {
+        super(subLevel, spawnX, spawnY);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
